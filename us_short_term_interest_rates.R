c2ac3b1c779dae3adbd46fef42af198b1514ed33
cordura21 <cordura21@gmail.com>
2023-03-08 17:37:28 -0300

    Added monthly interpolation
    In Shiller's case, each month is a linear interpolation of last december and next december.

--- us_short_term_interest_rates.R
+++ us_short_term_interest_rates.R
@@ -12,26 +12,36 @@ shiller_data <- na.omit(shiller_data)
 # Unify names
 names(fred_data_1) <- c('date','value')
 names(fred_data_2) <- c('date','value')
-names(shiller_data) <-  c('date','value')
+names(shiller_data) <-  c('year','value2')
 
-
-shiller_start <- as.Date(paste0(shiller_data$date[1],'-01-01'))
-shiller_end <-   as.Date(paste0(shiller_data$date[[NROW(shiller_data)]],'-01-01'))
-
-shiller_monthly <- data.frame(date = seq.Date(shiller_start,shiller_end,by = 'months'))
-
+# Convert from yyyy to yyyy-mm-dd
+library(dplyr)
 library(lubridate)
-shiller_monthly$year <- year(shiller_monthly$date)
+shiller_data$date <- Sys.Date()
+year(shiller_data$date) <-  shiller_data$year
+month(shiller_data$date) <-  12
+day(shiller_data$date) <- 1
+shiller_data$year <- NULL
+
+# Create a monthly data.frame
+shiller_monthly <- data.frame(date = seq.Date(first(shiller_data$date),
+                                              last(shiller_data$date),
+                                              by = 'months'))
 
 # Merge Shiller's yearly data with monthly frequency data.frame
-shiller_monthly <- merge(shiller_monthly,shiller_data, by.x = 'year', by.y = 'date')
-shiller_monthly$year <- NULL
+library(lubridate)
+shiller_monthly <- merge(shiller_monthly,shiller_data, by = 'date',all.x = TRUE)
+
+library(zoo)
+shiller_monthly$value <- na.approx(shiller_monthly$value2)
 
 # Bind everything together
 library(dplyr)
 
 fred_data_1$date <- as.Date(fred_data_1$date)
+fred_data_1$value2 <- NA
 fred_data_2$date <- as.Date(fred_data_2$date)
+fred_data_2$value2 <- NA
 
 fred_data_1$source <- 1
 fred_data_2$source <- 2
@@ -42,4 +52,4 @@ fred_data_1 <- fred_data_1 %>% filter(date < '1934-01-01')
 
 short_term_interest_rate <- bind_rows(shiller_monthly,fred_data_1,fred_data_2)
 
-write.csv(short_term_interest_rate,'us_short_term_rates.csv')
+write.csv(short_term_interest_rate,'Short rates series building/us_short_term_rates.csv', row.names = FALSE)
