2ae57a787782f169483271964e5d026b98887e73
cordura21 <cordura21@gmail.com>
2023-03-08 17:43:54 -0300

    Commented code

--- us_short_term_interest_rates.R
+++ us_short_term_interest_rates.R
@@ -14,23 +14,26 @@ names(fred_data_1) <- c('date','value')
 names(fred_data_2) <- c('date','value')
 names(shiller_data) <-  c('year','value2')
 
-# Convert from yyyy to yyyy-mm-dd
+# Convert Shiller's sheet from yyyy to yyyy-mm-dd
 library(dplyr)
 library(lubridate)
+
 shiller_data$date <- Sys.Date()
 year(shiller_data$date) <-  shiller_data$year
 month(shiller_data$date) <-  12
 day(shiller_data$date) <- 1
 shiller_data$year <- NULL
 
-# Create a monthly data.frame
+# Create a monthly date vector for Shiller's data
 shiller_monthly <- data.frame(date = seq.Date(first(shiller_data$date),
                                               last(shiller_data$date),
                                               by = 'months'))
 
-# Merge Shiller's yearly data with monthly frequency data.frame
-library(lubridate)
+# Merge Shiller's yearly data with monthly date vector
+
 shiller_monthly <- merge(shiller_monthly,shiller_data, by = 'date',all.x = TRUE)
+
+# Interpolate monthly rates from end-of-year rates
 
 library(zoo)
 shiller_monthly$value <- na.approx(shiller_monthly$value2)
@@ -43,13 +46,17 @@ fred_data_1$value2 <- NA
 fred_data_2$date <- as.Date(fred_data_2$date)
 fred_data_2$value2 <- NA
 
+# Document sources (see README.md on Github)
 fred_data_1$source <- 1
 fred_data_2$source <- 2
 shiller_monthly$source <- 3
 
+# Select dates from each data source
 shiller_monthly <- shiller_monthly %>% filter( date < '1920-01-01')
 fred_data_1 <- fred_data_1 %>% filter(date < '1934-01-01')
 
+# Bind everything and write file
 short_term_interest_rate <- bind_rows(shiller_monthly,fred_data_1,fred_data_2)
-
-write.csv(short_term_interest_rate,'Short rates series building/us_short_term_rates.csv', row.names = FALSE)
+write.csv(short_term_interest_rate,
+          'Short rates series building/us_short_term_rates.csv',
+          row.names = FALSE)
